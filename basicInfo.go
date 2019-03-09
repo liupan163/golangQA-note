99d222fc17caa8f45697213b2b3289e4a30db666
liupan163 <18792418363@163.com>
2019-03-09 14:39:46 +0800

    upd |> add new and make struct

--- basicInfo.go
+++ basicInfo.go
@@ -119,3 +119,14 @@ func NewFile(fd int, name string) *File {
 	return &f
 }
 */
+
+//new和make  区别用法
+var p *[]int = new([]int)       // allocates slice structure; *p == nil; rarely useful
+var v  []int = make([]int, 100) // the slice v now refers to a new array of 100 ints
+
+// Unnecessarily complex:
+var p *[]int = new([]int)
+*p = make([]int, 100, 100)
+
+// Idiomatic:
+v := make([]int, 100)
