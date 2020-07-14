abab2610a6375fcd0082d887f83414ed6ac47974
liupan163 <18792418363@163.com>
2020-07-14 08:59:39 +0800

    upd |> update return and defer code

--- interview_code/interview_code_2.go
+++ interview_code/interview_code_2.go
@@ -2,20 +2,26 @@ package main
 
 import "fmt"
 
-func init() {
-
-}
-
 /*
-	defer 跟 return
+defer、return、返回值三者的执行逻辑应该是：
+	return最先执行，return负责将结果写入返回值中；
+	接着defer开始执行一些收尾工作；
+	最后函数携带当前返回值退出。
+
+defer  延迟的函数参数就已经计算完成了
+
+执行顺序:先defer存参数，执行return语句，执行defer语句
+1、注意返回值，定义位置
+2、执行defer语句注意（参数是 值传递，还是引用传递）
 */
+
 func main() {
-	fmt.Println("tt1 return=", tt1())
-	fmt.Println("tt2 return=", tt2())
-	fmt.Println("tt3 return=", tt3())
-	fmt.Println("tt4 return=", tt4()())
+	//fmt.Println("tt1 return=", tt1())
+	//fmt.Println("tt2 return=", tt2())
+	//fmt.Println("tt3 return=", tt3())
+	//fmt.Println("tt4 return=", tt4()())
 	fmt.Println("tt5 return =", tt5())
-	fmt.Println("tt6 return =", tt6())
+	//fmt.Println("tt6 return =", tt6())
 }
 
 //return 1,defer tt1 0
@@ -37,7 +43,7 @@ func tt2() int {
 }
 
 //return 1,defer tt3 1
-func tt3() int {
+func tt3() int { //注意int 定义位置
 	var i = 0
 	defer func() {
 		fmt.Println("defer tt3", i) //引用变量
@@ -54,6 +60,7 @@ func tt4() func() int {
 	defer func() {
 		fmt.Println("defer tt4:", i)
 		i++
+		fmt.Println("defer tt4 after:", i)
 	}()
 	i++
 	return func() int {
